a4a826fc41468ae14bb12215b7cdc73a32b1387e
yuanxx <824264439@qq.com>
2018-03-28 18:00:45 +0800

    Signed-off-by: yuanxx <824264439@qq.com>

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 var config = require('common/script/config');
 App({
   onLaunch: function () {
-    this.getUserInfo();
+    var that = this;
     this.initStorage();
     wx.getStorage({
       key: 'person_info',
       success: function (res) {
         console.log(res);
+        if (res.data.id != null && res.data.updateTime != null) {
+          console.log('id and updateTime read ok');
+          var time = new Date();
+          var timeLag = (time - new Date(res.data.updateTime)) / 1000;
+          console.log('last updateTime is:' + res.data.updateTime);
+          console.log('The time is :' + timeLag);
+          if (timeLag < 120) {
+            return;
+          }
+        }
+        console.log('查询用户信息');
+        that.getUserInfo();
+        wx.getStorageInfo({
+          success: function (res) {
+            console.log(res);
+          },
+        })
       },
+      fail: function () {
+        console.log('查询失败');
+      }
     });
 
   },
@@ -24,22 +44,24 @@ App({
         that.globalData.userInfo = res.userInfo;
         wx.login({
           success: function (e) {
+            var nickName = that.globalData.userInfo.nickName;
+            var avatarUrl = that.globalData.userInfo.avatarUrl;
             wx.request({
               method: 'POST',
               url: 'http://127.0.0.1/member/getUserOpenId',
               data: {
                 code: e.code,
-                nickName: that.globalData.userInfo.nickName,
-                avatarUrl: that.globalData.userInfo.avatarUrl
+                nickName: nickName,
+                avatarUrl: avatarUrl
               },
               success: function (json) {
                 if (null != json.data.id && json.data.result == '1') {
                   //用户默认信息
                   var person_info = {
-                    nickName: '',
-                    avatarURL: '',
-                    id: '',
-                    updateTime: ''
+                    nickName: nickName,
+                    avatarURL: avatarUrl,
+                    id: json.data.id,
+                    updateTime: new Date()
                   }
                   //保存用户服务器返回的Id
                   wx.setStorage({
@@ -51,6 +73,14 @@ App({
             });
           }
         });
+      },
+      fail: function (e) {
+        console.log(e);
+        that.getUserInfo;
+        wx.showNavigationBarLoading();
+        wx.setNavigationBarTitle({
+          title: '正在加载用户信息',
+        })
       }
     })
   },
@@ -61,29 +91,37 @@ App({
 
   //方法：初始化缓存
   initStorage: function () {
-    wx.getStorageInfo({
-      success: function (res) {
+    // wx.getStorageInfoSync({
+    //   success: function (res) {
+    //     //用户默认信息
+    //     var person_info = {
+    //       nickName: null,
+    //       avatarURL: null,
+    //       id: null,
+    //       updateTime: null
+    //     }
+    //     console.log('初始化查询:' + res);
+    //     //判断用户Info是否存在
+    //     if (!('person_info' in res.keys)) {
+    //       console.log('person_info log is not exist');
+    //       wx.setStorageSync('person_info', '');
+    //     }
+    //   },
 
-        //用户默认信息
-        var person_info = {
-          nickName: '',
-          avatarURL: '',
-          id: '',
-          updateTime: ''
-        }
+    // });
 
-
-
-        //判断用户Info是否存在
-        if (!('person_info' in res.keys)) {
-          wx.setStorage({
-            key: 'person_info',
-            data: '',
-          });
-        }
-
-      },
-    })
-
+    var res = wx.getStorageInfoSync();
+    console.log(res);
+    console.log();
+    if (res.keys.indexOf('person_info') === -1) {
+      var person_info = {
+        nickName: null,
+        avatarURL: null,
+        id: null,
+        updateTime: null
+      }
+      //如果不存在缓存,新建缓存文件
+      wx.setStorageSync('person_info', person_info);
+    }
   },
 })
